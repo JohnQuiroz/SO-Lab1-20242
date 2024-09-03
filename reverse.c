3d9dfe09c85b620273e631041d8fd2b9f9254791
JohnQuiroz <bayron.quiroz@udea.edu.co>
2024-09-02 22:54:26 -0500

    Se modifica el archivo reverse.c para corregir el test 1

--- reverse.c
+++ reverse.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    printf("El clasico Hola Mundo\n");
+    switch(argc){
+        case 1:
+            printf("No se ingresaron parametros\n");
+        break;
+        case 2:
+            printf("Se ingreso un parametro\n");
+        break;
+        case 3:
+            printf("Se ingresaron dos parametros\n");
+        break;
+        default:
+            fprintf(stderr, "usage: reverse <input> <output>\n");
+            exit(1);
+        break;
+    }
     return 0;
 }
