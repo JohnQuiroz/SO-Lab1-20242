b19ac13413c9d7f6691c9408239107585b512969
JohnQuiroz <bayron.quiroz@udea.edu.co>
2024-09-09 21:21:35 -0500

    Se corrige el test 3

--- reverse.c
+++ reverse.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE *openFile(char *filename, char *mode) {
-    FILE *file = fopen(filename, mode);
-    if (file == NULL) {
-        fprintf(stderr, "reverse: cannot open file '%s'\n", filename);
-        exit(1);
-    }
-    return file;
-}
+FILE *openFile(char *, char *);
 
 int main(int argc, char *argv[])
 {
     FILE *input = NULL;
+    FILE *output = stdout;
     switch(argc){
         case 1:
             printf("No se ingresaron parametros\n");
@@ -23,7 +17,11 @@ int main(int argc, char *argv[])
             exit(0);
         break;
         case 3:
-            printf("Se ingresaron dos parametros\n");
+            input = openFile(argv[1] , "r");
+            output = openFile(argv[2], "w");
+            fclose(input);
+            fclose(output);
+            exit(0);
         break;
         default:
             fprintf(stderr, "usage: reverse <input> <output>\n");
@@ -31,4 +29,12 @@ int main(int argc, char *argv[])
         break;
     }
     return 0;
+}
+FILE *openFile(char *filename, char *mode) {
+    FILE *file = fopen(filename, mode);
+    if (file == NULL) {
+        fprintf(stderr, "reverse: cannot open file '%s'\n", filename);
+        exit(1);
+    }
+    return file;
 }
